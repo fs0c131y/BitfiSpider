ecc8b30871acd542171f197dc6759a4b5c44ddb6
Baptiste Robert <bapti.robert@gmail.com>
2019-07-15 11:25:39 +0200

    Initial contribution

---
+++ README.md
@@ -0,0 +1,37 @@
+# Bitfi Scrapy Spider
+The Bitfi sources are locate at [bitfi.dev](https://bitfi.dev/NoxMessages/Articles.aspx?Category=sources). Analyse the sources directly on the website is impossible and there is no versioning at all.
+
+With this Scrapy spider you will get the latest sources from [bitfi.dev](https://bitfi.dev)
+
+## Prerequisites
+* Python 2.7
+* Scrapy
+
+## How to install
+
+Clone this repository
+
+```shell
+git clone https://github.com/fs0c131y/BitfiSpider.git
+```
+
+Then install the dependencies
+
+```shell
+sudo pip install -r requirements.txt
+```
+
+## How to run it
+Run the following command:
+
+```shell
+scrapy crawl thebitfi
+```
+
+The latest source code will be stored in the `output` folder under the current directory.
+
+## Contact
+Follow me on [Twitter](https://twitter.com/fs0c131y)! You can also find a small part of my work at [https://fs0c131y.com](https://fs0c131y.com)
+
+## Credits
+This script has been made with ❤ ️by @fs0c131y
---
+++ TheBitfi/spiders/thebitfi.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import os
+import errno
+import scrapy
+
+
+class ThebitfiSpider(scrapy.Spider):
+    name = 'thebitfi'
+    allowed_domains = ['bitfi.dev']
+    start_urls = [
+        'https://bitfi.dev/NoxMessages/Articles.aspx?Category=sources']
+
+    def parse(self, response):
+        if response.status == 200:
+            files = response.xpath(
+                '//*[contains(@class,"faq-section")]/a/@href')
+            for file in files:
+                url = 'https://bitfi.dev/NoxMessages/' + file.get()
+                yield scrapy.Request(url, self.parse_file)
+
+    def parse_file(self, response):
+        if response.status == 200:
+            #print(response.text)
+            title = response.xpath('//*[contains(@class,"faq-section")]/h2/text()').get()
+            subtitle = response.xpath('//*[contains(@class,"faq-section")]/h3/text()').get().rsplit('\\', 1)[0][1:].replace('\\', '/')
+            code = response.xpath('//*[contains(@class,"faq-code")]/text()').get()
+
+            try:  
+                os.makedirs('output/' + subtitle)
+            except OSError as exc:
+                if exc.errno == errno.EEXIST and os.path.isdir(subtitle):
+                    pass
+                else:
+                    raise
+
+            with open('output/' + subtitle + '/' + title, 'a') as f:
+                f.write(code)
